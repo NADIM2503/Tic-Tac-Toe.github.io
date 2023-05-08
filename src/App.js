61fec6dbdb5029ba979054a74a354654d71d5021
NADIM2503 <122076180+NADIM2503@users.noreply.github.com>
2023-05-08 14:21:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Games from './Components/Games';
+
+function App() {
+  return (
+    <>
+    <Games/>
+    </>
+  );
+}
+
+export default App;
